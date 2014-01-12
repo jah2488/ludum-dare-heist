dda1abfdbf7992caa1ffcdfa6ed7a44293bad99e
Jeremy White <dudymas@gmail.com>
2014-01-11 19:38:21 -0500

    added alarm that listens to alarm manager

--- Assets/Scripts/Flash.cs
+++ Assets/Scripts/Flash.cs
@@ -5,18 +5,28 @@ public class Flash : MonoBehaviour
 {
 		bool down;
 		Light light;
+		bool alerted;
+
+		void AlertToggle(Vector3 pos){
+			//alerted = !alerted; //this doesn't work!
+			alerted = true;
+			Debug.Log ("Set light:alerted to be " + alerted);
+		}
 
 		void Start ()
 		{
-				light = GetComponent<Light>();
+			light = GetComponent<Light>();
+			alerted = false;
+			AlarmManager.OnOhSnap += AlertToggle;
 		}
 
 		void Update ()
 		{
-				if(light.intensity < 0.45) {
+			if (alerted) {
+				if (light.intensity < 0.45) {
 						down = false;
 				}
-				if(light.intensity > 2.0) {
+				if (light.intensity > 2.0) {
 						down = true;
 				}
 
@@ -25,5 +35,6 @@ public class Flash : MonoBehaviour
 				} else {
 						light.intensity += 1f * Time.deltaTime;
 				}
+			}
 		}
 }
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+		AlertOnPlayer alert = null;
+
+		// Use this for initialization
+		void Start () {
+			if (transform != null && transform.parent != null)
+				alert = transform.parent.GetComponent<AlertOnPlayer> ();
+		}
+
+		// Update is called once per frame
+		void Update () {
+			
+		}
+
+		void OnTriggerEnter(Collider other) {
+			if(other.tag == "Player" && alert != null) {
+				alert.TriggerEntered();
+			}
+		AlarmManager.Alert();
+		}
+
+		void OnTriggerExit(Collider other) {
+			if(other.tag == "Player" && alert != null) {
+				alert.TriggerExit();
+			}
+		}
+
+}
