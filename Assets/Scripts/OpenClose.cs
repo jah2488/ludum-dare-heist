4b9d28ad8a9d35cd75a74b63254ccb74b432a5db
Justin Herrick <justin@justinherrick.com>
2014-01-12 14:33:31 -0500

    Adding Open/Close script to handle 'opening' doors by lowering them and then 'closing' them by raising them up to their original height.

---
+++ Assets/Scripts/OpenClose.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenClose : MonoBehaviour {
+
+		public bool OpeningOrClosing;
+		public bool Opened;
+		public float Distance;
+		public float ClosedPositionY;
+
+		void Start () {
+					ClosedPositionY = transform.localPosition.y;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+					Distance = Mathf.Abs(transform.localPosition.y - ClosedPositionY);
+					if(OpeningOrClosing) {
+							if(Opened) {
+									if(transform.localPosition.y >= ClosedPositionY) { Opened = false; OpeningOrClosing = false;}
+									transform.Translate(Vector3.up);
+							} else {
+									if(Distance > 2 * transform.localScale.y) { Opened = true; OpeningOrClosing = false;}
+									transform.Translate(Vector3.down);
+							}
+					} else {
+							if(!Opened) {
+								transform.localPosition = new Vector3(transform.localPosition.x, ClosedPositionY, transform.localPosition.z);
+//								transform.localPosition.Set(transform.localPosition.x, ClosedPositionY, transform.localPosition.z);
+							}
+					}
+		}
+
+		public void TriggerOpenClose() {
+				OpeningOrClosing = true;
+		}
+}
